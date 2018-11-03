cd4dda9c7baa4866846a28ddde865542b5786622
Vallabh Kansagara <vrkansagara@gmail.com>
2018-11-03 12:13:32 +0530

    Adding ping api for api
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,31 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/ping',function(){
+    return Carbon::now();
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['middleware' => ['role:super-admin']], function () {
+    //
+});
+
+Route::group(['middleware' => ['permission:publish articles']], function () {
+    //
+});
+
+Route::group(['middleware' => ['role:super-admin','permission:publish articles']], function () {
+    //
+});
+
+Route::group(['middleware' => ['role_or_permission:super-admin']], function () {
+    //
+});
+
+Route::group(['middleware' => ['role_or_permission:publish articles']], function () {
+    //
 });
