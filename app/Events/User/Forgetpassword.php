d8346acfe106c71626e5b6aa9f90df43208a82ea
Vallabh Kansagara <vrkansagara@gmail.com>
2018-11-24 13:54:36 +0530

    Adding events for the user and it's trait.
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

---
+++ app/Events/User/Forgetpassword.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events\User;
+
+use App\Traits\EventTraits;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class Forgetpassword
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    use EventTraits;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ app/Traits/EventTraits.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vallabh
+ * Date: 24/11/18
+ * Time: 1:52 PM
+ */
+
+namespace App\Traits;
+
+
+trait EventTraits
+{
+
+}
