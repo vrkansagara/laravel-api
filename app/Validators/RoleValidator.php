a4200e8447a9c51595f2eff641319a786045d21c
Vallabh Kansagara <vrkansagara@gmail.com>
2018-12-16 23:44:09 +0530

    Rules added for the roles.
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- app/Validators/RoleValidator.php
+++ app/Validators/RoleValidator.php
@@ -2,6 +2,7 @@
 
 namespace App\Validators;
 
+use Illuminate\Contracts\Validation\Factory;
 use \Prettus\Validator\Contracts\ValidatorInterface;
 use \Prettus\Validator\LaravelValidator;
 
@@ -12,13 +13,23 @@ use \Prettus\Validator\LaravelValidator;
  */
 class RoleValidator extends LaravelValidator
 {
+
     /**
      * Validation Rules
      *
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'name' => 'required|min:3|max:191|unique:roles',
+            'display_name' => 'required|min:3|max:191',
+            'guard_name' => 'required|min:3|max:191',
+        ],
         ValidatorInterface::RULE_UPDATE => [],
+    ];
+
+
+    protected $messages = [
+        'required' => 'The :attribute field is required.',
     ];
 }
