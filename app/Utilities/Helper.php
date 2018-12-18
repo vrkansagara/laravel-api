365a288ea342dda3486783266d965ade2cdbd7aa
Vallabh Kansagara <vrkansagara@gmail.com>
2018-12-18 20:29:26 +0530

    additional helper added.
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- app/Utilities/Helper.php
+++ app/Utilities/Helper.php
@@ -1,13 +1,13 @@
 <?php
 
-if (!function_exists('get_ip_address')) {
+if (!function_exists('getIpAddress')) {
 
     /**
      * Retrieves the best guess of the client's actual IP address.
      * Takes into account numerous HTTP proxy headers due to variations
      * in how different ISPs handle IP addresses in headers between hops.
      */
-    function get_ip_address()
+    function getIpAddress()
     {
         // check for shared internet/ISP IP
         if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
@@ -40,13 +40,13 @@ if (!function_exists('get_ip_address')) {
     }
 }
 
-if (!function_exists('validate_ip')) {
+if (!function_exists('isValidIp')) {
 
     /**
      * Ensures an ip address is both a valid IP and does not fall within
      * a private network range.
      */
-    function validate_ip($ip)
+    function isValidIp($ip)
     {
         if (strtolower($ip) === 'unknown') {
             return false;
@@ -72,4 +72,109 @@ if (!function_exists('validate_ip')) {
         return true;
     }
 
+}
+
+
+if (!function_exists('randcomString')) {
+    /**
+     * @param number
+     * @return string
+     */
+    function randcomString($length = 16)
+    {
+        $capitalLatters = substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 1);
+        $smallLatters = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz'), 0, 1);
+        $number = substr(str_shuffle('0123456789'), 0, 1);
+        $specialCharacters = substr(str_shuffle("!#$%&*+-<=>@_|"), 0, 1);
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*+-<=>@_|';
+        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length - 4) . $capitalLatters . $smallLatters . $number . $specialCharacters;
+    }
+}
+
+
+if (!function_exists('httpStatusMessage')) {
+    /**
+     * @param int $code
+     * @return string
+     */
+    function httpStatusMessage($code)
+    {
+        /**
+         * 1×× Informational
+         * 2×× Success
+         * 3×× Redirection
+         * 4×× Client Error
+         * 5×× Server Error
+         */
+        $statusCodes = [
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            102 => 'Processing',
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            207 => 'Multi-Status',
+            208 => 'Already Reported',
+            226 => 'IM Used',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            307 => 'Temporary Redirect',
+            308 => 'Permanent Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Payload Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            418 => 'I\'m a teapot',
+            421 => 'Misdirected Request',
+            422 => 'Unprocessable Entity',
+            423 => 'Locked',
+            424 => 'Failed Dependency',
+            426 => 'Upgrade Required',
+            428 => 'Precondition Required',
+            429 => 'Too Many Requests',
+            431 => 'Request Header Fields Too Large',
+            444 => 'Connection Closed Without Response',
+            451 => 'Unavailable For Legal Reasons',
+            499 => 'Client Closed Request',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported',
+            506 => 'Variant Also Negotiates',
+            507 => 'Insufficient Storage',
+            508 => 'Loop Detected',
+            510 => 'Not Extended',
+            511 => 'Network Authentication Required',
+            599 => 'Network Connect Timeout Error',
+        ];
+        if (!in_array($code, $statusCodes, true)) {
+            return $statusCodes[$code];
+        } else {
+            return 'Something went wrong.';
+        }
+    }
 }
