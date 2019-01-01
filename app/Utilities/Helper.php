0e5cdc31f884283d84a1304c86fa30cf3642cd2d
Vallabh Kansagara <vrkansagara@gmail.com>
2019-01-01 16:01:29 +0530

    Adding RSA and AES encryption helper method

--- app/Utilities/Helper.php
+++ app/Utilities/Helper.php
@@ -74,24 +74,6 @@ if (!function_exists('isValidIp')) {
 
 }
 
-
-if (!function_exists('randcomString')) {
-    /**
-     * @param number
-     * @return string
-     */
-    function randcomString($length = 16)
-    {
-        $capitalLatters = substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 1);
-        $smallLatters = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz'), 0, 1);
-        $number = substr(str_shuffle('0123456789'), 0, 1);
-        $specialCharacters = substr(str_shuffle("!#$%&*+-<=>@_|"), 0, 1);
-        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*+-<=>@_|';
-        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length - 4) . $capitalLatters . $smallLatters . $number . $specialCharacters;
-    }
-}
-
-
 if (!function_exists('httpStatusMessage')) {
     /**
      * @param int $code
@@ -178,3 +160,156 @@ if (!function_exists('httpStatusMessage')) {
         }
     }
 }
+if (!function_exists('isValidJson')) {
+    /**
+     * @param string $string
+     * @return boolean
+     */
+    function isValidJson($string)
+    {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+}
+
+
+if (!function_exists('generateRandomString')) {
+
+    /**
+     * Generate random string.
+     * @param int $length
+     * @return string
+     */
+    function generateRandomString($length = 32)
+    {
+        // Alphabets (Capitals & Smalls), numeric values and special characters should be there
+        $capital = substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 1);
+        $small = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz'), 0, 1);
+        $number = substr(str_shuffle('0123456789'), 0, 1);
+        $specialCharacters = substr(str_shuffle("!#$%&*-@_"), 0, 1);
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*-@_';
+        $string = substr(str_shuffle(str_repeat($pool, 5)), 0, $length - 2) . $capital . $small . $number . $specialCharacters;
+        return $string;
+    }
+}
+
+if (!function_exists('generateRsaEncryption')) {
+
+    /**
+     * @param $string
+     * @param $publicKeyPath
+     * @return string
+     */
+    function generateRsaEncryption($string, $publicKeyPath)
+    {
+        $fp = fopen($publicKeyPath, 'r');
+        $pub_key = fread($fp, 8192);
+        fclose($fp);
+        openssl_public_encrypt($string, $crypttext, $pub_key);
+        return base64_encode($crypttext);
+    }
+}
+
+if (!function_exists('generateAesEncryption')) {
+
+    /**
+     * @param $string
+     * @param $keyString
+     * @param int $isDecrypt
+     * @return string
+     */
+    function generateAesEncryption($string, $keyString, $isDecrypt = 0)
+    {
+        /**
+         * CBC has an IV and thus needs randomness every time a message is encrypted
+         * openssl_get_cipher_methods($method)
+         * // Most secure key
+         * $key = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
+         *
+         */
+        $method = 'aes-256-ecb';
+        $ivlen = openssl_cipher_iv_length($method);
+        // Most secure iv Never ever use iv=0 in real live. Better use this:
+        $iv = openssl_random_pseudo_bytes($ivlen);
+        $string = base64_encode(openssl_encrypt($string, $method, $keyString, OPENSSL_RAW_DATA, $iv));
+        if ($isDecrypt) {
+            $string = openssl_decrypt(base64_decode($string), $method, $keyString, OPENSSL_RAW_DATA, $iv);
+        }
+        return $string;
+    }
+}
+
+if (!function_exists('connect')) {
+
+    /**
+     * @param $url
+     * @param string $method
+     * @param array $payload
+     * @param array $headers
+     * @param int $isDebug
+     * @return array
+     */
+    function connect($url, $method = 'get', array $payload, $headers = [], $isDebug = 0)
+    {
+        $timeout = 30;
+        $redirect = 3;
+        $verboseLog = '';
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            return [
+                'status' => 0,
+                'response' => null,
+                'verbose' => $verboseLog
+            ];
+        }
+        $curl = curl_init();
+        $userAgent = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31';
+        $options = [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_USERAGENT => $userAgent,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_CONNECTTIMEOUT => $timeout,
+            CURLOPT_MAXREDIRS => $redirect,
+            CURLOPT_ENCODING => "",
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        ];
+
+        if (strtolower($method) == 'post') {
+            $options[CURLOPT_POST] = 1;
+            $options[CURLOPT_POSTFIELDS] = json_encode($payload);
+        }
+
+        if (!is_null($headers)) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        }
+
+        if ($isDebug) {
+            curl_setopt($curl, CURLOPT_VERBOSE, true);
+            $verbose = fopen('php://temp', 'w+');
+            $options[CURLOPT_VERBOSE] = true;
+            $options[CURLOPT_STDERR] = $verbose;
+        }
+        curl_setopt_array($curl, $options);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        $server_output = curl_exec($curl);
+
+        if ($isDebug) {
+            rewind($verbose);
+            $verboseLog = stream_get_contents($verbose);
+        }
+        curl_close($curl);
+
+        if (!$server_output) {
+            die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
+        }
+        $returnResponse = [
+            'status' => 1,
+            'response' => $server_output,
+            'verbose' => $verboseLog
+        ];
+
+        return $returnResponse;
+
+    }
+}
+
